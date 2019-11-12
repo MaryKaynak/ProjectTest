6babb2c6176925b5b7ee6fd2a945638d955dee08
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-11 23:42:50 -0500

    test

--- src/TestProject.java
+++ src/TestProject.java
@@ -6,7 +6,7 @@ public class TestProject {
     public int sum(int num1, int num2) {
         int total = 0;
         // write your code here
-
+           total = num1 + num2;
         // end of code
         return total;
     }
@@ -17,7 +17,7 @@ public class TestProject {
     public int division(int num3, int num4) {
         int result = 0;
         // write your code here
-
+           result = num3 / num4;
         // end of code
         return result;
     }
@@ -28,7 +28,7 @@ public class TestProject {
     public int multiplication(int i1, int i2) {
         int result = 0;
         // write your code here
-
+           result = i1 * i2;
         // end of code
         return result;
     }
@@ -39,7 +39,7 @@ public class TestProject {
     public int totalCharacters(String myStr) {
         int stringLength = 0;
         // write your code here
-
+        stringLength = myStr.length();
         // end of code
         return stringLength;
     }
